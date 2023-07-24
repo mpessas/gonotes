99367fa1b51de1474288c81c3b274bcbc0395550
Apostolis Bessas <apostolis@bessas.me>
2023-07-24 13:27:05 +0300

    Use custom type for note IDs
    Using a prefix will allow us to more easily identify those IDs in e.g.
    logs.

--- pkg/domain/note.go
+++ pkg/domain/note.go
@@ -2,18 +2,38 @@ package domain
 
 import "github.com/GoWebProd/uuid7"
 
+type NoteId string
+
+func NoteIdFromUuid(v uuid7.UUID) NoteId {
+	return NoteId("note_" + v.String())
+}
+
+func (v *NoteId) String() string {
+	return string(*v)
+}
+
+func (v *NoteId) StringUUID() string {
+	return string(*v)[5:]
+}
+
 type Note struct {
-	Id     string `json:"id"`
+	Id     NoteId `json:"id"`
 	Body   string `json:"body"`
 	Author Author `json:"author"`
 }
 
+type NoteRepository interface {
+	Add(note *Note)
+}
+
 func newNote(body string, author Author) *Note {
-	id := uuid7.New().Next().String()
+	id := NoteIdFromUuid(uuid7.New().Next())
 	return &Note{id, body, author}
 }
 
-func CreateNotePort(body, author_s string) *Note {
+func CreateNotePort(r NoteRepository, body, author_s string) *Note {
 	author := newAuthor(author_s)
-	return newNote(body, *author)
+	note := newNote(body, *author)
+	r.Add(note)
+	return note
 }
--- pkg/domain/note_test.go
+++ pkg/domain/note_test.go
@@ -5,6 +5,14 @@ import (
 	"testing"
 )
 
+type MemNoteRepository struct {
+	Notes []Note
+}
+
+func (r *MemNoteRepository) Add(note *Note) {
+	r.Notes = append(r.Notes, *note)
+}
+
 func TestCreateNotePort(t *testing.T) {
 	var tests = []struct {
 		name  string
@@ -17,14 +25,23 @@ func TestCreateNotePort(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			ans := CreateNotePort(tt.input[0], tt.input[1])
+			r := MemNoteRepository{}
+
+			ans := CreateNotePort(&r, tt.input[0], tt.input[1])
 			if ans.Body != tt.want[0] || ans.Author != Author(tt.want[1]) {
 				t.Errorf("got %s, want %s", *ans, tt.want)
 			}
-			if _, err := uuid7.Parse(ans.Id); err != nil {
+			if _, err := uuid7.Parse(ans.Id.StringUUID()); err != nil {
 				t.Errorf("invalid ID %s", ans.Id)
 			}
 
+			if len(r.Notes) != 1 {
+				t.Errorf("note not added in repository")
+			}
+
+			if r.Notes[0] != *ans {
+				t.Errorf("difference between repo and returned value")
+			}
 		})
 	}
 }
